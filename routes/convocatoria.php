1e7627fc4f75aa2c5ab0258563636b62da2c1269
kleber <muruchi2018@gmail.com>
2025-05-13 11:07:06 -0400

    ruta convocatoria

--- routes/convocatoria.php
+++ routes/convocatoria.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 // Rutas públicas para convocatorias
 Route::get('/convocatorias', [ConvocatoriaController::class, 'publicadas'])->name('convocatoria.publica');
 Route::get('/convocatorias/{id}', [ConvocatoriaController::class, 'verPublica'])->name('convocatoria.publica.ver');
+Route::get('/convocatorias/{id}/pdf', [ConvocatoriaController::class, 'exportarPdf'])->name('convocatorias.exportarPdf');
 
 // Add these routes for exports
 Route::get('/convocatoria/exportar/pdf', [ConvocatoriaController::class, 'exportPdf'])->name('convocatoria.exportar.pdf');
@@ -27,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/convocatoria/export/excel', [ConvocatoriaController::class, 'exportExcel'])->name('convocatorias.exportExcel');
     
     // Export specific convocatoria to PDF
-    Route::get('/convocatoria/{id}/pdf', [ConvocatoriaController::class, 'exportarPdf'])->name('convocatorias.exportarPdf');
+    //Route::get('/convocatoria/{id}/pdf', [ConvocatoriaController::class, 'exportarPdf'])->name('convocatorias.exportarPdf');
     
     // Edit Convocatoria routes
     Route::get('/convocatoria/{id}/editar', [ConvocatoriaController::class, 'edit'])->name('convocatorias.editar');
